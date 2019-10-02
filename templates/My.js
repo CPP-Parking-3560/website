6e23a3a03565d5743eaba79e5f0da37e76998eeb
CarolineResler <carolineresler2@gmail.com>
2019-10-01 18:33:52 -0700

    js file for user profile

---
+++ templates/My.js
@@ -0,0 +1,14 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyA8bOv1jNRZwyX59w9mqZYom2joLCJWFqQ",
+    authDomain: "cs3560parking.firebaseapp.com",
+    databaseURL: "https://cs3560parking.firebaseio.com",
+    projectId: "cs3560parking",
+    storageBucket: "cs3560parking.appspot.com",
+    messagingSenderId: "270171142840",
+    appId: "1:270171142840:web:34eab48113259ab11a524d",
+    measurementId: "G-5QZBZF3NFB"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
