3ddbe2bfd4a92b79f2aa6b906b91335adab22826
CarolineResler <carolineresler2@gmail.com>
2019-10-15 14:59:41 -0700

    update   2

--- templates/My.js
+++ templates/My.js
@@ -8,9 +8,10 @@ var firebaseConfig = {
     projectId: "cs3560parking",
     storageBucket: "cs3560parking.appspot.com",
     messagingSenderId: "270171142840",
-    appId: "1:270171142840:web:057b2cc6e33ef0df1a524d",
-    measurementId: "G-05KBYW6DPR"
+    appId: "1:270171142840:web:620cb57b803ba3981a524d",
+    measurementId: "G-TVNM1KFV8F"
 };
+
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 firebase.analytics();
